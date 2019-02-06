5cec8177a3c0d853c13821dbe3c807b91c710937
Nishant Chahar <nishantc.co.17@nsit.net.in>
2019-02-06 18:13:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,67 @@ app.get("/blogs",function(req,res){
 	
 });
 
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
+});
+
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+   var formData = req.body.blog;
+   Blog.create(formData, function(err, newBlog){
+       console.log(newBlog);
+      if(err){
+          res.render("new");
+      } else {
+          res.redirect("/blogs");
+      }
+   });
+});
+
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+      if(err){
+          res.redirect("/");
+      } else {
+          res.render("show", {blog: blog});
+      }
+   });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+       if(err){
+           console.log(err);
+           res.redirect("/")
+       } else {
+           res.render("edit", {blog: blog});
+       }
+   });
+});
+
+app.put("/blogs/:id", function(req, res){
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog){
+       if(err){
+           console.log(err);
+       } else {
+         var showUrl = "/blogs/" + blog._id;
+         res.redirect(showUrl);
+       }
+   });
+});
+
+app.delete("/blogs/:id", function(req, res){
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       }else{
+           res.redirect("/blogs");
+       }
+   }); 
+});
+
+
+
 app.listen(process.env.PORT,process.env.IP,function(){
 	console.log("server is running");
 });
